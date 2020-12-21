00ae968e5cd1e832143bf845e0e087aae27b090b
guoliwei <guoliwei@longshine.com>
2020-12-21 09:26:42 +0800

    Merge branch 'master' of https://github.com/kukume/kuku-bot
     Conflicts:
    	src/main/java/me/kuku/yuq/event/GroupEvent.java

---
+++ src/main/java/me/kuku/yuq/controller/genshin/GenShinController.java
@@ -0,0 +1,29 @@
+package me.kuku.yuq.controller.genshin;
+
+import com.IceCreamQAQ.Yu.annotation.Action;
+import com.icecreamqaq.yuq.annotation.QMsg;
+import com.icecreamqaq.yuq.entity.Contact;
+import me.kuku.yuq.controller.ark.ArkService;
+
+import javax.inject.Inject;
+
+public class GenShinController {
+    @Inject
+    GenShinService genShinService;
+
+    @Action("{pool}单抽")
+    @QMsg(at = true)
+    public String cardOne(Contact qq, String pool) {
+        //根据QQ号查询池子
+        //根据得到的池子信息抽卡
+        //返回信息
+
+        return null;
+    }
+
+    @Action("{pool}十连")
+    @QMsg(at = true)
+    public String cardTen(Contact qq, String pool) {
+        return null;
+    }
+}
---
+++ src/main/java/me/kuku/yuq/controller/genshin/GenShinService.java
@@ -0,0 +1,4 @@
+package me.kuku.yuq.controller.genshin;
+
+public class GenShinService {
+}
